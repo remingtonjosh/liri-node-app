41286d0ef05e53778a2a7a5bd80bdc03acd729ef
Remington Villa <remingtonvilla@gmail.com>
2019-10-14 12:51:43 -0500

    uplod

--- README.md
+++ README.md
@@ -1,22 +1,68 @@
 ![Logo](IMG_6563.PNG)
 
 # LIRI
-## LIRI will search:
-### *Spotify for songs
-### *Bands in Town for concerts
-### *and OMDB for movies
+
+### LIRI is your personal assistant for songs, concerts and movie information all in one app.
+
+#
+
+## HOW TO USE LIRI
+
+| To search for:                            | Use Command line:                                  |
+| ----------------------------------------- | -------------------------------------------------- |
+| Songs and samples through Spotify         | node liri.js spotify-this-song _(SONG TITLE HERE)_ |
+| Concerts near you through Bands In Town   | node liri.js concert-this _(ARTIST HERE)_          |
+| Movies and information through OMDB       | node liri.js movie-this _(MOVIE HERE)_             |
+| \*Do What I Say feature (Preset commands) | node liri.js do-what-it-says                       |
+
+#
+
+## What Each Command Should Do
+
+#
+
+### node liri.js concert-this _(ARTIST HERE)_
+
+This will search the Bands in Town Artist Events API ("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp") for an artist and render the following information about each event to the terminal:
+
+- Name of the venue
+
+- Venue location
+
+- Date of the Event
+
+#
+
+### node liri.js spotify-this-song _(SONG TITLE HERE)_
+
+This will search the Spotify API for song title information. We are also using a JS file to store and hide our keys for security purposes. This command will show the following information about the song in your terminal/bash window:
+
+- Artist(s)
+- The song's name
+- A preview link of the song from Spotify
+- The album that the song is from
+
+#
+
+### node liri.js movie-this _(MOVIE HERE)_
+
+This will search the The IMDB API for specific movie information. This command will show the following information about the song in your terminal/bash window:
+
+- Title of the movie.
+- Year the movie came out.
+- IMDB Rating of the movie.
+- Rotten Tomatoes Rating of the movie.
+- Country where the movie was produced.
+- Language of the movie.
+- Plot of the movie.
+- Actors in the movie.
+
+#
+
+### node liri.js do-what-it-says
+
+Using the fs Node package, LIRI will take the text inside of random.txt and then use it to call one of LIRI's commands.
+
+#
 
 ![screenshot place holder](IMG_6563.PN)
-
-
-Clearly state the problem the app is trying to solve (i.e. what is it doing and why)
-Give a high-level overview of how the app is organized
-Give start-to-finish instructions on how to run the app
-Include screenshots, gifs or videos of the app functioning
-Contain a link to a deployed version of the app
-Clearly list the technologies used in the app
-State your role in the app development
-
-
-
-
--- liri.js
+++ liri.js
@@ -1,3 +1,96 @@
+// "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
+
+// axios installed // bands in town and  spotify.node.api
+
 require("dotenv").config();
+
 var keys = require("./keys.js");
+var Spotify = require("node-spotify-api");
 var spotify = new Spotify(keys.spotify);
+var axios = require("axios");
+var fs = require("fs");
+var moment = require("moment");
+var action = process.argv[2];
+var value = process.argv.slice(3).join(" ");
+
+switch (action) {
+  case "concert-this":
+    concertThis();
+    break;
+  case "spotify-this-song":
+    spotifyThisSong();
+    break;
+  case "movie-this":
+    movieThis();
+    break;
+  case "do-what-it-says":
+    doWhatItSays();
+    break;
+  default:
+    break;
+    console.log("this is not recognized");
+}
+function concertThis() {
+  var artist = value;
+  console.log(artist);
+  axios
+    .get(
+      "https://rest.bandsintown.com/artists/" +
+        artist +
+        "/events?app_id=codingbootcamp"
+    )
+    .then(function(response) {
+      console.log("--- Concert Information ---");
+      console.log("Venue: " + response.data[0].venue.name);
+      console.log("City: " + response.data[0].venue.city);
+      console.log(
+        "Date: " + moment(response.data[0].datetime).format("MM/DD/YYYY")
+      );
+      console.log(
+        "https://rest.bandsintown.com/artists/" +
+          artist +
+          "/events?app_id=codingbootcamp"
+      );
+      // console.log(response.data);
+    });
+}
+function movieThis() {
+  var movie = value;
+  axios
+    .get("http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy")
+    .then(function(response) {
+      console.log("--- YOUR MOVIE INFO ---");
+      console.log("Movie Title: " + response.data.Title);
+      console.log("Movie Year Released: " + response.data.Year);
+      console.log("IMDB Rating: " + response.data.imdbRating);
+      console.log("Rotten Tomato Rating: " + response.data.Ratings[1].Value);
+      console.log("Country Produced In: " + response.data.Country);
+      console.log("Language: " + response.data.Language);
+      console.log("Actors: " + response.data.Actors);
+      console.log("Plot: " + response.data.Plot);
+      console.log(
+        "http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy"
+      );
+    });
+}
+
+function spotifyThisSong() {
+  // var track = value;
+  // console.log(track);
+  spotify.search({ type: "track", query: value }, function(err, data) {
+    if (err) {
+      return console.log("Error occurred: " + err);
+    }
+    console.log("--- SPOTIFY TRACK FOUND ---");
+    console.log("Artist: " + data.tracks.items[0].artists[0].name);
+    console.log("Song: " + data.tracks.items[0].name);
+    console.log("Album: " + data.tracks.items[0].album.name);
+    console.log("--- SPOTIFY ALBUM SAMPLE ---");
+    console.log(data.tracks.items[0].album.external_urls);
+  });
+}
+
+function doWhatItSays() {
+  var contents = fs.readFileSync("random.txt", "utf8");
+  console.log(contents);
+}
